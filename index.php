9fd11db37225674c4486a94372d878ef54d2a7d4
elaiyarasan <milayarasan@gmail.com>
2018-10-17 17:44:47 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Buy Lifes Amazing Prize</title>
+	<link rel="shortcut icon" href="image/title.png"/>
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
+
+
+<style>
+	.block
+	{
+		display:inline-flex;
+	}
+</style>
+</head>
+<body>
+<header style="background-color:#232f3d;width:100%;height:auto;">
+	<nav>
+		<div class="block">
+		<img src="image/amazon.png" style="height:48px;width:99px;padding:14px 0px 0px 18px">
+	
+			<div class="block" style="padding-left:30px;margin:18px 0px 0px 0px">
+				<select style="border-radius:3px 0px  0px 3px;width:90px;height:34px">
+				<option>Books</option>
+				<option>cd</option>
+				<option>Mobile</option>
+				<option>home</option>
+			</select>
+			<input type="" name="" style="width:815px;height:28px;"><button style="background-color:#f3a847;border-color:#f3a847;width:43px;height:34px"><i class="fa fa-search"></i></button> 
+			</div>
+			<div class="block">
+				<img src="image/amz.png" style="width:43px;height:34px;padding:19px 0px 0px 42px;border-radius:2px"><h1 style="color:white;">AMAZON APP <span style="color:#f3a847;">JACKPOT</span></h1><i class="fa fa-angle-right" style="color:#f3a847; font-size:70px;padding-bottom:20px"></i>
+			</div>
+		</div>
+			</nav>
+			<div class="block">
+				
+				<i class="fas fa-map-marker-alt" style="font-size:25px;color:white;padding: 0px 0px 5px 30px;margin-top:-23px;"></i><p  style="font-size:15px;color:white;padding: 0px 0px 5px 30px;margin-top:-23px;">Deliver to <br>Chennai 600001‌</p>
+				<span  style="font-size:15px;color:white;padding: 0px 0px 5px 30px;margin-top:-23px;">Short By Category</span><select>
+					<option></option>
+				</select>
+			
+			</div>
+</header>
+</body>
+</html>
